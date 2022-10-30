4109759c5cef45d575204b3ad89ad28871152bd3
Merdasa-Belay <merdasabelay@gmail.com>
2022-10-30 20:44:35 +0300

    test console file fix

--- tests/test_console.py
+++ tests/test_console.py
@@ -507,8 +507,8 @@ class TestHBNBCommand_destroy(unittest.TestCase):
             self.assertFalse(HBNBCommand().onecmd("destroy City"))
             self.assertEqual(correct, output.getvalue().strip())
         with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(testCmd="Amenity.update({testId}, attr_name)"
-                             HBNBCommand().onecmd("destroy Amenity"))
+            self.assertFalse(testCmd="Amenity.update({}, attr_name)".format(testId)
+HBNBCommand().onecmd("destroy Amenity"))
             self.assertEqual(correct, output.getvalue().strip())
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("destroy Place"))
@@ -1079,7 +1079,7 @@ class TestHBNBCommand_update(unittest.TestCase):
             self.assertEqual(correct, output.getvalue().strip())
 
     def test_update_missing_attr_value_space_notation(self):
-        self.assertFalse(HBNBCommand().onecmd(testCmd))
+            self.assertFalse(HBNBCommand().onecmd(testCmd))
         correct = "** value missing **"
         with patch("sys.stdout", new=StringIO()) as output:
             HBNBCommand().onecmd("create BaseModel")
